13ead938c668d1b31d97a4255263ca2834764efb
rioles <rioles1992@gmail.com>
2023-11-10 11:41:22 +0100

    Remove ignored files and update .gitignore

--- build/lib/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from models.engine.dbs_manager import DBSManager
-from repository.hotel_reservation_crud_port import HotelReservationCrudPort
-storage: HotelReservationCrudPort = DBSManager()
-#storage = DBSManager()
-storage.reload()
